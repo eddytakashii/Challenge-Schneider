d8101438713c850c4b403ebf9a04dd40229ebdce
eddytakashii <nakauchi.takashi@gmail.com>
2023-07-28 20:09:51 -0300

    update/1

--- js/gamificacao.js
+++ js/gamificacao.js
@@ -19,6 +19,23 @@ function clicou(){
 
 /*Gamificação Chuveiro*/
 
+/*Gamificação Luz*/
+function calcular(){
+  var valorInput = document.getElementById('valor').value
+  var resposta = document.getElementById('resposta')
+  
+  resposta.innerHTML = valorInput;
+
+  if (valorInput > 0 && valorInput < 10) {
+    resposta.innerHTML = 'Vc esta sendo bem economico';
+  } else if (valorInput >= 10 && valorInput < 20) {
+    resposta.innerHTML = 'Vc está sendo moderado na sua economia';
+  } else {
+    resposta.innerHTML = 'Vc está gastando muito';
+  }
+  
+}
+/*Gamificação Luz*/
 
 /*Gamificação Economia para o seu bolso conta de Luz*/
 function custoMensal(){
