1e88e07fe1dc06feeeb75532a5f701a4bc33d416
eddytakashii <nakauchi.takashi@gmail.com>
2023-08-14 18:49:35 -0300

    update

--- js/gamificacao.js
+++ js/gamificacao.js
@@ -31,12 +31,12 @@ function calcular(){
   
   resposta.innerHTML = valorInput;
 
-  if (valorInput > 0 && valorInput < 10) {
-    resposta.innerHTML = 'Vc esta sendo bem economico';
-  } else if (valorInput >= 10 && valorInput < 20) {
-    resposta.innerHTML = 'Vc está sendo moderado na sua economia';
+  if (valorInput > 0 && valorInput < 600) {
+    resposta.innerHTML = 'Em muitas regiões, um consumo mensal de energia elétrica de até cerca de 500 kWh a 600 kWh pode ser considerado baixo para uma casa de tamanho médio. Isso, é claro, pode variar com base nas condições locais.';
+  } else if (valorInput >= 600 && valorInput < 1000) {
+    resposta.innerHTML = 'Um consumo médio mensal de energia elétrica, que incluiria a maioria das residências, geralmente varia entre 600 kWh a 1000 kWh, dependendo das mesmas considerações regionais e fatores já mencionados.';
   } else {
-    resposta.innerHTML = 'Vc está gastando muito';
+    resposta.innerHTML = 'Um consumo mensal de energia elétrica acima de 1000 kWh a 1500 kWh (ou mais) pode ser considerado alto para uma casa de tamanho médio, novamente dependendo das condições locais.';
   }
   
 }
@@ -49,14 +49,15 @@ function custoMensal(){
   var custoAnual = 12*custo
   var economia = (12*custo) - (20*(12*custo)/100);
   var porcentagem = (20*(12*custo)/100);
+  var dezanos = 10*porcentagem
   resultado.innerHTML = custo
 
-  if (custo >0 && custo <60){
-    resultado.innerHTML = `O valor que gasta por ano é ${custoAnual}.Parabéns, vc está mantendo sua economia em ordem.`
-  } else if (custo >=60 && custo <=120){
-    resultado.innerHTML = `O valor que vc gasta por ano é ${custoAnual}. Você pode chegar a um gasto de até ${economia}, que equivale a uma economia de até ${porcentagem} todo ano! Aqui estão algumas soluçoes para que você possa gaurdar mais dinheiro:`
+  if (custo >0 && custo <220){
+    resultado.innerHTML = `O valor que gasta por ano é ${custoAnual} reais.Parabéns, vc está mantendo sua economia em ordem.`
+  } else if (custo >=220 && custo <=450){
+    resultado.innerHTML = `O valor que vc gasta por ano é ${custoAnual} reais. Tomando algumas medidas preventivas, você pode chegar a um gasto de até ${economia} reais, que equivale a uma economia de até ${porcentagem} reais todo ano!`
   } else{
-    resultado.innerHTML = `O valor que vc gasta por ano é ${custoAnual}. Você está gastando muito. Se tomar medidas mais preventivas, vc pode estar salvando até ${porcentagem} reais todo ano. Em 10 anos, você pode comprar...`
+    resultado.innerHTML = `O valor que vc gasta por ano é ${custoAnual} reais. Você está gastando muito. Se tomar medidas mais preventivas, você pode estar salvando até ${porcentagem} reais todo ano! Imagine daqui a 10 anos, você estaria salvando cerca de ${dezanos} reais! `
   }
 }
 /*Gamificação Economia para o seu bolso conta de Luz*/
